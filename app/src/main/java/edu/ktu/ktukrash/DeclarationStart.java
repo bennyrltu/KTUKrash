20c09dce74e0fd9e4a9e9d8b51eee86b089b7b50
bennyrltu <78347616+bennyrltu@users.noreply.github.com>
2021-10-17 13:37:24 +0300

    Moved buttons a little

---
+++ app/src/main/java/edu/ktu/ktukrash/DeclarationStart.java
@@ -0,0 +1,14 @@
+package edu.ktu.ktukrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeclarationStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_declaration_start);
+    }
+}
