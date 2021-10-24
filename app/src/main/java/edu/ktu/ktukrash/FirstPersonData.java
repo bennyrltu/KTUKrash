7909c9c8bcf87122b246ff456971f330262f3649
tehFruit <76218849+tehFruit@users.noreply.github.com>
2021-10-24 04:34:50 +0300

    Location button

--- app/src/main/java/edu/ktu/ktukrash/FirstPersonData.java
+++ app/src/main/java/edu/ktu/ktukrash/FirstPersonData.java
@@ -2,11 +2,17 @@ package edu.ktu.ktukrash;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +22,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.TypeFilter;
@@ -23,14 +33,23 @@ import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.hbb20.CountryCodePicker;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class FirstPersonData extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private CountryCodePicker ccp;
     private TextView phoneTextView;
     private Button button;
+
+    //Location stuff
+    private Button locationButton;
+    private EditText locationText;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    //--------------------------------
 
     public static final String EXTRA_TEXT3 = "ktu.edu.KTUKrash.EXTRA.TEXT3";
 
@@ -66,6 +85,41 @@ public class FirstPersonData extends AppCompatActivity implements DatePickerDial
                 datePicker.show(getSupportFragmentManager(), "date picker");
             }
         });
+
+        //Location stuff
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationButton = (Button) findViewById(R.id.buttonLocation);
+        locationText = (EditText) findViewById(R.id.addressField);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(FirstPersonData.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            Location location = task.getResult();
+                            if(location != null){
+                                Geocoder geocoder = new Geocoder(FirstPersonData.this,
+                                        Locale.getDefault());
+                                try {
+                                    List<Address> addresses = geocoder.getFromLocation(
+                                            location.getLatitude(), location.getLongitude(), 1
+                                    );
+                                    locationText.setText(addresses.get(0).getAddressLine(0));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+                }
+                else{
+                    ActivityCompat.requestPermissions(FirstPersonData.this
+                            ,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+        //---------------------------------------------------
 
 
     }
