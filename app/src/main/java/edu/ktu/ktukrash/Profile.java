e42fa16de1b70a08c8e0bd3b683220f2302b5f7d
beviltiskas <78384738+beviltiskas@users.noreply.github.com>
2021-11-06 18:45:40 +0200

    fixed profile UI, gradient doesn't apply to button

--- app/src/main/java/edu/ktu/ktukrash/Profile.java
+++ app/src/main/java/edu/ktu/ktukrash/Profile.java
@@ -1,6 +1,9 @@
 package edu.ktu.ktukrash;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,11 +23,20 @@ public class Profile extends AppCompatActivity {
     private FirebaseUser user;
     private DatabaseReference reference;
     private String userID;
+    private Button listButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        listButton = findViewById(R.id.declarationList);
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Profile.this, "To be done!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users");
