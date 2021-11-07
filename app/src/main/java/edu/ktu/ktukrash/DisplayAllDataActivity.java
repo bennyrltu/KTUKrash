1f04da19ca6a611ab3fea9ffabbac1e8426fd5f9
B <benasrudnc@gmail.com>
2021-11-07 13:14:36 +0200

    Added declaration count at the bottom to display.

--- app/src/main/java/edu/ktu/ktukrash/DisplayAllDataActivity.java
+++ app/src/main/java/edu/ktu/ktukrash/DisplayAllDataActivity.java
@@ -1,20 +1,29 @@
 package edu.ktu.ktukrash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DisplayAllDataActivity extends AppCompatActivity {
 
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
 
+    TextView textView;
+
     String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
 
     MainAdapter mainAdapter;
 
@@ -23,6 +32,8 @@ public class DisplayAllDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_all_data);
+
+        textView = (TextView) findViewById(R.id.textView12);
 
         recyclerView = (RecyclerView) findViewById(R.id.rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -35,6 +46,20 @@ public class DisplayAllDataActivity extends AppCompatActivity {
 
         mainAdapter = new MainAdapter(options);
         recyclerView.setAdapter(mainAdapter);
+
+        DatabaseReference fbDb = FirebaseDatabase.getInstance().getReference().child("Declaration_Data").child(currentuser).child("Declarations");
+        fbDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int size = (int) snapshot.getChildrenCount();
+                textView.setText("Declaration count: " + size);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
