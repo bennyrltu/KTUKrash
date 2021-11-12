3d3770ed9ddca8fbaa392cb411218e6ee0779ec7
Marsku1 <79592172+Marsku1@users.noreply.github.com>
2021-11-12 22:44:45 +0200

    Major UI change

--- app/src/main/java/edu/ktu/ktukrash/LoginFragment.java
+++ app/src/main/java/edu/ktu/ktukrash/LoginFragment.java
@@ -6,6 +6,7 @@ import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -29,6 +30,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_main, container, false);
+
 
         Button signIn = view.findViewById(R.id.signIn);
         signIn.setOnClickListener(this);
